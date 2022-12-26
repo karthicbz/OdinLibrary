1da92bf58a635b9654b2432e92cb8582bb3f2efd
Karthick P <pkarthicbz@live.in>
2022-12-26 17:11:50 +0530

    make add button add book details into container

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+const bookName = document.querySelector('#bookName');
+const authorName = document.querySelector('#authorName');
+const bookPages = document.querySelector('#pages');
+const readStatus = document.querySelector('#read');
+const addButton = document.querySelector('.addBook');
+
+const container = document.querySelector('.container');
+
 let myLibrary = [];
 
 function Book(name, author, pages, read){
@@ -11,6 +19,23 @@ function Book(name, author, pages, read){
 function addBookToLibrary(){
     myLibrary.push(new Book(name, author, pages, read));
 }
+
+addButton.addEventListener('click', (e)=>{
+    container.innerHTML = '';
+    e.preventDefault();
+    Book(bookName.value, authorName.value, bookPages.value, readStatus.value);
+    addBookToLibrary();
+
+    for (let i=0; i<myLibrary.length; i++){
+        const div = document.createElement('div');
+        for(let key in myLibrary[i]){
+            const p = document.createElement('p');
+            p.textContent = `${key}: ${myLibrary[i][key]}`;
+            div.appendChild(p);
+        }
+        container.appendChild(div);
+    }
+});
 
 
 
