eb08b398cb006e7396e01c5498660efa092bd63d
Karthick P <pkarthicbz@live.in>
2022-12-26 17:40:34 +0530

    add fleex  to form

--- script/script.js
+++ script/script.js
@@ -20,21 +20,30 @@ function addBookToLibrary(){
     myLibrary.push(new Book(name, author, pages, read));
 }
 
+addBookToLibrary.prototype.createCard = function(arr){
+    for (let i=0; i<arr.length; i++){
+        const div = document.createElement('div');
+        for(let key in arr[i]){
+            const p = document.createElement('p');
+            p.textContent = `${key}: ${arr[i][key]}`;
+            div.appendChild(p);
+        }
+        const delButton = document.createElement('button');
+        delButton.classList.add('delButton');
+        delButton.textContent='delete';
+        div.appendChild(delButton);
+        container.appendChild(div);
+    }
+}
+
 addButton.addEventListener('click', (e)=>{
     container.innerHTML = '';
     e.preventDefault();
     Book(bookName.value, authorName.value, bookPages.value, readStatus.value);
     addBookToLibrary();
 
-    for (let i=0; i<myLibrary.length; i++){
-        const div = document.createElement('div');
-        for(let key in myLibrary[i]){
-            const p = document.createElement('p');
-            p.textContent = `${key}: ${myLibrary[i][key]}`;
-            div.appendChild(p);
-        }
-        container.appendChild(div);
-    }
+    addBookToLibrary.prototype.createCard(myLibrary);
+    
 });
 
 
